1693fb49749190ab4ec7f829c130506e2601250c
brijean <brijean@cisco.com>
2021-06-02 13:50:51 -0400

    Added improved error handling

--- get_devicelist.py
+++ get_devicelist.py
@@ -33,6 +33,12 @@ def getAPNeighborDetails(dnac,apIPList):
    sum_api_calls = 0
 
    for ip in apIPList:
+     skip_flag = False
+     no_neighbor = False
+     ap_neighbor_row = {}
+     if ip == '128.107.234.18':
+        print("\nTime for some debug\n")
+     
      headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
@@ -45,7 +51,6 @@ def getAPNeighborDetails(dnac,apIPList):
      #adding some sleep time to see if we can get around DNAC API Rate Limiting
      if len(apIPList) > 5 :
          time.sleep(13)
-     sum_api_calls = sum_api_calls + 1
      print("Getting details for:", ip, "\n")
      response =  requests.get(url, headers=headers, verify=False ).json()
 
@@ -56,31 +61,60 @@ def getAPNeighborDetails(dnac,apIPList):
      elif 'error' in response in response:
        print(response['error'])
        return()
+     elif 'errorDescription' in response:
+       print(response['errorDescription'])
+       skip_flag = True
+     elif len(response[0]['deviceDetails']['neighborTopology']) < 1:
+        no_neighbor = True
+#     else:
+#        device = (response[0]["deviceDetails"])
+     
+     if skip_flag == True:
+       ap_neighbor_row['hostname'] = None
+       ap_neighbor_row['IP Address'] = ip
+       ap_neighbor_row['MAC Address'] = None
+       ap_neighbor_row['WLC IP'] = None
+       ap_neighbor_row['neighbor_ip'] = None
+       ap_neighbor_row['neighbor_int'] = None        
      else:
-        device = (response[0]["deviceDetails"])
-     
-     #get relevant AP attribures
-     ap_neighbor_row = {'hostname': device['hostname'], 'IP Address': device['managementIpAddress'], 'MAC Address': device['macAddress'].lower(), 'WLC IP': device['associatedWlcIp']}
-     
-     #iterate through nodes and links tables to find CDP neighbor information
-     topo_dict = device['neighborTopology'][0]
-     for table_id, table_info in topo_dict.items():
-       if table_id == 'nodes':
-         for item in table_info:
-           if item['additionalInfo']['macAddress'] != None and item['additionalInfo']['macAddress'].lower() == ap_neighbor_row['MAC Address'].lower():
-              #found the AP's ID
-              ap_id = item['id']
-           if item['family'] == 'Switches and Hubs':
-              ap_neighbor_row['neighbor'] = item['name']
-              ap_neighbor_row['neighbor_ip'] = item['ip']
-              #found the cdp neighbor switch ip
-              switch_id = item['id']
-       elif table_id == 'links':
-         for item in table_info:
-            if item['target'] == switch_id and item['source'] == ap_id:
-               ap_neighbor_row['neighbor_int'] = item['targetInterfaceName']
-         ap_neighbor_table.append(ap_neighbor_row)
+       device = (response[0]["deviceDetails"])
+       #get relevant AP attributes
+       ap_neighbor_row = {'hostname': device['hostname'], 'IP Address': device['managementIpAddress'], 'MAC Address': device['macAddress'].lower(), 'WLC IP': device['associatedWlcIp']}
+       if no_neighbor == False:     
+         #iterate through nodes and links tables to find CDP neighbor information
+         topo_dict = device['neighborTopology'][0]
+         if 'nodes' in topo_dict and 'links' in topo_dict:
+           for table_id, table_info in topo_dict.items():
+             if table_id == 'nodes':
+               for item in table_info:
+                 #initialize switch_id
+                 switch_found = False
+                 if item['additionalInfo']['macAddress'] != None and item['additionalInfo']['macAddress'].lower() == ap_neighbor_row['MAC Address'].lower():
+                   #found the AP's ID
+                   ap_id = item['id']
+                 if item['family'] == 'Switches and Hubs':
+                   ap_neighbor_row['neighbor'] = item['name']
+                   ap_neighbor_row['neighbor_ip'] = item['ip']
+                   #found the cdp neighbor switch ip
+                   switch_found = True
+                   switch_id = item['id']
+             elif table_id == 'links':
+               for item in table_info:
+                 if switch_found:
+                   if item['target'] == switch_id and item['source'] == ap_id:
+                     ap_neighbor_row['neighbor_int'] = item['targetInterfaceName']
+             if switch_found == False:
+               #no switch neighbor information avaiable
+               ap_neighbor_row['neighbor'] = None
+               ap_neighbor_row['neighbor_ip'] = None
+               ap_neighbor_row['neighbor_int'] = None
+       else:
+         ap_neighbor_row['neighbor'] = None
+         ap_neighbor_row['neighbor_ip'] = None
+         ap_neighbor_row['neighbor_int'] = None
+     ap_neighbor_table.append(ap_neighbor_row.copy())
    return(ap_neighbor_table)   
+
 
 def print_csv(listOfDict, fName):
   csvColumns = listOfDict[0].keys()
@@ -103,8 +137,12 @@ if __name__ == "__main__":
    #get target AP list for report
    ap_list = getAPList(dnac_parameters['host'])
    #run ap neighbor report on ap_list
-   ap_neighbor_report = getAPNeighborDetails(dnac_parameters['host'],ap_list)
+   if len(ap_list) >= 1:
+      ap_neighbor_report = getAPNeighborDetails(dnac_parameters['host'],ap_list)
+      #produce AP Neighbor report
+      report_name = 'ap_neighbor_report.csv'
+      #print report to a CSV file
+      print_csv(ap_neighbor_report, 'ap_neighbor_report.csv')
+   else:
+      print("There no APs in inventory on", dnac_parameters['host'])
 
-   #output report
-   report_name = 'ap_neighbor_report.csv'
-   print_csv(ap_neighbor_report, 'ap_neighbor_report.csv')
